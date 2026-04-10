9b2f53ada8f0fcd112ff30e9786a85f7780ee702
hsenani-dev <75816777+hsenani-dev@users.noreply.github.com>
2026-04-10 15:38:46 -0700

    Update neovim.nix

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -24,7 +24,6 @@ in
         {
           plugin = config.catppuccin.sources.nvim;
           type = "lua";
-          doCheck = false;
           config = ''
             lua << EOF
               local compile_path = vim.fn.stdpath("cache") .. "/catppuccin-nvim"
